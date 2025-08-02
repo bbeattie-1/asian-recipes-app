5f92157bba78675751727020397b36615dbe9c18
bbeattie <bbeatter@gmail.com>
2025-08-02 08:55:18 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+import AppNavigation from './components/AppNavigation.vue';
+
+</script>
+
+<template>
+
+<calcite-shell>
+  <AppNavigation />
+  <div>
+    <p>Pad Lad Na</p>
+    <ul>
+      <li>1 Packet Jasmine Rice</li>
+      <li>1 Chicken Breast</li>
+      <li>1 Carrot</li>
+      <li>1 Zucchini</li>
+      <li>1/2 Cup Chicken Stock</li>
+      <li>3 Spoonfuls Soy Sauce</li>
+      <li>1 Spoonfuls Oyster Sauce</li>
+      <li>1 Spoonfuls Butter</li>
+      <li>1 Spoonfuls Corn Starch Slurry</li>
+    </ul>
+    <p>Mongolian Beef</p>
+    <ul>
+      <li>1 Packet Jasmine Rice</li>
+      <li>1 Top Round Steak</li>
+      <li>1 Green Onions</li>
+      <li>1/2 Cup Brown Sugar</li>
+      <li>3 Spoonfuls Soy Sauce</li>
+      <li>1 Spoonfuls Dark Soy Sauce</li>
+      <li>1 Spoonfuls Hoison Sauce</li>
+      <li>1 Spoonfuls Garlic</li>
+      <li>1 Spoonfuls Ginger</li>
+    </ul>
+  </div>
+</calcite-shell>
+
+</template>
+
+<style scoped>
+
+p, ul {
+  color: black;
+  margin-left: 1rem;
+}
+
+</style>
---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <calcite-navigation slot="header">
+        <calcite-navigation-logo slot="logo" icon="language" heading="East Meets West" description=" Asian-inspired recipes from Ben"  />
+        <calcite-action @click="$emit('share-click')" slot="content-end" icon="share" scale="m"></calcite-action>
+    </calcite-navigation>
+
+</template>
+
+<style scoped>
+
+* {
+    --calcite-color-foreground-1: var(--calcite-color-brand);
+    --calcite-icon-color: #ffffff;  
+}
+
+</style>
