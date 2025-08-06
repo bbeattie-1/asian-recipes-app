8b25f44ba0f3274026a5d9a8bb9cbd03ad4f35b9
bbeattie <bbeatter@gmail.com>
2025-08-06 07:48:23 -0400

    Added Pad See Ew

--- src/App.vue
+++ src/App.vue
@@ -9,17 +9,28 @@ import AppNavigation from './components/AppNavigation.vue';
 <calcite-shell>
   <AppNavigation />
   <div>
-    <p>Pad Lad Na</p>
+    <p>Pad Lad Na (Rice Version)</p>
     <ul>
       <li>1 Packet Jasmine Rice</li>
       <li>1 Chicken Breast</li>
       <li>1 Carrot</li>
       <li>1 Zucchini</li>
       <li>1/2 Cup Chicken Stock</li>
-      <li>3 Spoonfuls Soy Sauce</li>
-      <li>1 Spoonfuls Oyster Sauce</li>
-      <li>1 Spoonfuls Butter</li>
-      <li>1 Spoonfuls Corn Starch Slurry</li>
+      <li>3 Spoonful(s) Soy Sauce</li>
+      <li>1 Spoonful(s) Minced Garlic</li>
+      <li>1 Spoonful(s) Oyster Sauce</li>
+      <li>1 Spoonful(s) Butter</li>
+      <li>1 Spoonful(s) Corn Starch Slurry</li>
+    </ul>
+    <p>Pad See Ew</p>
+    <ul>
+      <li>1 Packet Rice Sheets</li>
+      <li>3 Eggs</li>
+      <li>3 Spoonful(s) Soy Sauce</li>
+      <li>1 Spoonful(s) Dark Soy Sauce</li>
+      <li>1 Spoonful(s) Oyster Sauce</li>
+      <li>1/4 Spoonful(s) Rice Vinegar</li>
+      <li>1/4 Spoonful(s) Fish Sauce</li>
     </ul>
     <p>Mongolian Beef</p>
     <ul>
@@ -27,11 +38,11 @@ import AppNavigation from './components/AppNavigation.vue';
       <li>1 Top Round Steak</li>
       <li>1 Green Onions</li>
       <li>1/2 Cup Brown Sugar</li>
-      <li>3 Spoonfuls Soy Sauce</li>
-      <li>1 Spoonfuls Dark Soy Sauce</li>
-      <li>1 Spoonfuls Hoison Sauce</li>
-      <li>1 Spoonfuls Garlic</li>
-      <li>1 Spoonfuls Ginger</li>
+      <li>3 Spoonful(s) Soy Sauce</li>
+      <li>1 Spoonful(s) Dark Soy Sauce</li>
+      <li>1 Spoonful(s) Hoison Sauce</li>
+      <li>1 Spoonful(s) Garlic</li>
+      <li>1 Spoonful(s) Ginger</li>
     </ul>
   </div>
 </calcite-shell>
